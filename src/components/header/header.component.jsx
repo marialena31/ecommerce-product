78b68ba720ae8372007662433fbe397255156845
Maria'lena Pietri <marialena.pietri@maps-system.com>
2021-11-30 19:46:34 +0100

    first commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { ReactComponent as IconCart } from '../../assets/icon-cart.svg'
+import './header.styles.scss';
+
+const Header = () => {
+   
+    const CartIcon = () => {
+        <IconCart className="shopping-icon" />
+    }
+
+    const ProfileImage = () => {
+        <img src="../../assets/image-avatar.png" alt="profile" name="profile image" />
+    }
+
+    return (
+        <header id="header" className="header">
+            <Link to="/">
+                <Logo className="header__nav__logo" />
+            </Link>
+            <nav className="header__nav">
+                <Link className="header" to='/collections'>
+                    Collections
+                </Link>
+                <Link className="header" to='/men'>
+                    Men
+                </Link>
+                <Link className="header" to='/women'>
+                    Women
+                </Link>
+                <Link className="header" to='/about'>
+                    About
+                </Link>
+                <Link className="header" to='/contact'>
+                    Contact
+                </Link>
+            </nav>
+             <CartIcon />
+            <ProfileImage/> 
+        </header>
+    )
+};
+
+export default Header;
+
